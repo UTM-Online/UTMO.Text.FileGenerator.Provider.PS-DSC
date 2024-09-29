bc3bace4a6c1678cdfc00d8ea881bce1308a94cd
Josh Irwin <joshua@irwin.ws>
2024-09-29 14:05:35 -0700

    Checking in changes

---
+++ src/UTMO.Text.FileGenerator.Provider.DSC/Resources/Choco/ChocoPackageSourceDsc.cs
@@ -0,0 +1,38 @@
+﻿namespace UTMO.Text.FileGenerator.Provider.DSC.Resources.Choco;
+
+public class ChocoPackageSourceDsc : DscConfigurationItem
+{
+    public ChocoPackageSourceDsc(string name) : base(name)
+    {
+        this.PropertyBag["Name"] = string.Empty;
+        this.PropertyBag["Source"] = string.Empty;
+    }
+
+    public string SourceName
+    {
+        get
+        {
+            return this.PropertyBag["Name"];
+        }
+        
+        set
+        {
+            this.PropertyBag["Name"] = value;
+        }
+    }
+    
+    public string SourceUri
+    {
+        get
+        {
+            return this.PropertyBag["Source"];
+        }
+        
+        set
+        {
+            this.PropertyBag["Source"] = value;
+        }
+    }
+
+    public override string ResourceId => "cChocoSource";
+}
---
+++ src/UTMO.Text.FileGenerator.Provider.DSC/Resources/FileSystem/FileDsc.cs
@@ -0,0 +1,38 @@
+﻿namespace UTMO.Text.FileGenerator.Provider.DSC.Resources.FileSystem;
+
+public class FileDsc : DscConfigurationItem
+{
+    public FileDsc(string name) : base(name)
+    {
+        this.PropertyBag["DestinationPath"] = string.Empty;
+        this.PropertyBag["Contents"] = string.Empty;
+    }
+
+    public string DestinationPath
+    {
+        get
+        {
+            return this.PropertyBag["DestinationPath"];
+        }
+        
+        set
+        {
+            this.PropertyBag["DestinationPath"] = value;
+        }
+    }
+    
+    public string Contents
+    {
+        get
+        {
+            return this.PropertyBag["Contents"];
+        }
+        
+        set
+        {
+            this.PropertyBag["Contents"] = value;
+        }
+    }
+
+    public override string ResourceId => "File";
+}
