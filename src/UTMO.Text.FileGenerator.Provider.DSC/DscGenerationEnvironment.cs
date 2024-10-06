eb4c81a1847b5007fb45d37c308291fba30b21ac
Josh Irwin <joshua@irwin.ws>
2024-10-06 13:51:52 -0700

    Adding generation environments

---
+++ src/UTMO.Text.FileGenerator.Provider.DSC/DscGenerationEnvironment.cs
@@ -0,0 +1,13 @@
+﻿namespace UTMO.Text.FileGenerator.Provider.DSC;
+
+public sealed class DscGenerationEnvironment : DscGenerationEnvironmentBase
+{
+    private DscGenerationEnvironment(string templatePath) : base(templatePath)
+    {
+    }
+    
+    public static DscGenerationEnvironment Create(string templatePath)
+    {
+        return new DscGenerationEnvironment(templatePath);
+    }
+}
---
+++ src/UTMO.Text.FileGenerator.Provider.DSC/DscGenerationEnvironmentBase.cs
@@ -0,0 +1,22 @@
+﻿namespace UTMO.Text.FileGenerator.Provider.DSC;
+
+using UTMO.Text.FileGenerator.Provider.DSC.Models;
+
+public abstract class DscGenerationEnvironmentBase : GenerationEnvironmentBase
+{
+    protected DscGenerationEnvironmentBase(string templatePath) : base(templatePath)
+    {
+    }
+
+    protected DscGenerationEnvironmentBase AddComputer<T>() where T : DscComputer, new()
+    {
+        this.AddResource<T>(new T());
+        return this;
+    }
+
+    protected DscGenerationEnvironmentBase AddConfiguration<T>() where T : DscConfiguration, new()
+    {
+        this.AddResource<T>(new T());
+        return this;
+    }
+}
