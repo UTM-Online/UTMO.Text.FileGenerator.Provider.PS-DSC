1336b9831730050109432146275e72bff8d726be
Josh Irwin <joshua@irwin.ws>
2024-11-23 11:49:15 -0800

    Adding missing method to IDscResourceConfig.cs

--- src/UTMO.Text.FileGenerator.Provider.DSC.Abstract/Contracts/IDscResourceConfig.cs
+++ src/UTMO.Text.FileGenerator.Provider.DSC.Abstract/Contracts/IDscResourceConfig.cs
@@ -1,5 +1,6 @@
 ﻿namespace UTMO.Text.FileGenerator.Provider.DSC.Abstract.Contracts;
 
+using UTMO.Text.FileGenerator.Provider.DSC.Abstract.BaseTypes;
 using UTMO.Text.FileGenerator.Provider.DSC.Abstract.Enums;
 
 public interface IDscResourceConfig
@@ -7,4 +8,6 @@ public interface IDscResourceConfig
     DscEnsure Ensure { get; set; }
     
     List<string> DependsOn { get; }
+
+    DscConfigurationItem AddDependency<T>(T resource) where T : DscConfigurationItem;
 }
