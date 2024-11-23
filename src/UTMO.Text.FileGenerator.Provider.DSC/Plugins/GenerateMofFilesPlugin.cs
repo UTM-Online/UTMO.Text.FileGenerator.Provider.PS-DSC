87ad662e45415e501fded0629eb5c000a2d69928
Josh Irwin <joshua@irwin.ws>
2024-11-23 13:03:26 -0800

    Adding plugin for generating mof files

---
+++ src/UTMO.Text.FileGenerator.Provider.DSC/Plugins/GenerateMofFilesPlugin.cs
@@ -0,0 +1,31 @@
+﻿namespace UTMO.Text.FileGenerator.Provider.DSC.Plugins;
+
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using UTMO.Text.FileGenerator.Abstract;
+using UTMO.Text.FileGenerator.Provider.DSC.Abstract.BaseTypes;
+
+public class GenerateMofFilesPlugin : IRenderingPipelinePlugin
+{
+    public GenerateMofFilesPlugin(IGeneralFileWriter writer, ITemplateGenerationEnvironment environment)
+    {
+        this.Writer = writer;
+        this.Environment = environment;
+    }
+
+    public void HandleTemplate(ITemplateModel model)
+    {
+        var scriptConfig  = model.ProduceOutputPath(this.Environment.OutputPath);
+        var fileName      = model.ResourceName;
+        var fileType      = model.GetType() == typeof(DscLcmConfiguration) ? "Configuration" : "Computers";
+        var mofOutputFile = Path.GetFullPath(Path.Join(scriptConfig, @"..\..", "MOF", fileType, $"{fileName}.mof"));
+
+        using var ps = PowerShell.Create(InitialSessionState.CreateDefault2());
+        ps.AddCommand($"{scriptConfig} -OutputPath {mofOutputFile}");
+        ps.Invoke();
+    }
+
+    public IGeneralFileWriter Writer { get; init; }
+
+    public ITemplateGenerationEnvironment Environment { get; init; }
+}
