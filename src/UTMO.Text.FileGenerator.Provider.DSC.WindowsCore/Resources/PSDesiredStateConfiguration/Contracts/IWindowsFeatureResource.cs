ce147d6f54705f6a2a1f6bde3a4c3687ccf8871e
Josh Irwin <joshua@irwin.ws>
2025-01-19 14:54:30 -0800

    Updating resource contract

--- src/UTMO.Text.FileGenerator.Provider.DSC.WindowsCore/Resources/PSDesiredStateConfiguration/Contracts/IWindowsFeatureResource.cs
+++ src/UTMO.Text.FileGenerator.Provider.DSC.WindowsCore/Resources/PSDesiredStateConfiguration/Contracts/IWindowsFeatureResource.cs
@@ -5,4 +5,6 @@ using UTMO.Text.FileGenerator.Provider.DSC.Abstract.Contracts;
 public interface IWindowsFeatureResource : IDscResourceConfig
 {
     string FeatureName { get; set; }
+    
+    string Source { get; set; }
 }
