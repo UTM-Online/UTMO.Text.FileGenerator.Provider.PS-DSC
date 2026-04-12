4d29b61e2c29c8cc5193d19eba575d778047c9ea
Josh Irwin <joshua@irwin.ws>
2026-04-11 18:06:13 -0700

    feat: update DSC CLI options to make restoring and repackaging modules optional

--- src/UTMO.Text.FileGenerator.Provider.DSC/Models/DscCliOptions.cs
+++ src/UTMO.Text.FileGenerator.Provider.DSC/Models/DscCliOptions.cs
@@ -11,9 +11,9 @@ public class DscCliOptions : GeneratorCliOptions
     [Option('c', "IsCiCd", Required = false, HelpText = "Indicates if the DSC configuration is being generated for a CI/CD pipeline.", Default = false)]
     public bool IsCiCd { get; set; }
     
-    [Option('r', "restoreRequiredModules", Required = false, HelpText = "Indicates whether to restore required DSC modules before generating MOF files.", Default = true)]
+    [Option('r', "restoreRequiredModules", Required = false, HelpText = "Indicates whether to restore required DSC modules before generating MOF files.", Default = false)]
     public bool RestoreRequiredModules { get; set; }
     
-    [Option('p', "repackageRequiredModules", Required = false, HelpText = "Indicates whether to repackage required DSC modules after restoring them.", Default = true)]
+    [Option('p', "repackageRequiredModules", Required = false, HelpText = "Indicates whether to repackage required DSC modules after restoring them.", Default = false)]
     public bool RepackageRequiredModules  { get; set; }
 }
